c6e35f8e00cc070c3c10df01626d26fe67f8bc8e
ezntek <eason@ezntek.com>
2024-02-11 18:51:02 +0800

    back up

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+hyprpm reload &
 waybar &
 dunst &
+gentoo-pipewire-launcher &
+#/usr/local/libexec/xdg-desktop-portal -v -r &
+#/usr/local/libexec/xdg-desktop-portal-hyprland -l DEBUG &
+
+/home/ezntek/.local/bin/waypaper --restore
