a31ad17b6cb9395577a068c59e0a03263c8e919e
ezntek <eason@ezntek.com>
2024-03-19 20:53:13 +0800

    post-upgrade things

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -3,8 +3,8 @@
 dinit &
 hyprctl setcursor Qogir 24 &
 hyprpm reload &
-nextcloud &
 #/usr/local/libexec/xdg-desktop-portal -v -r &
 #/usr/local/libexec/xdg-desktop-portal-hyprland -l DEBUG &
 kdeconnect-indicator &
+#easyeffects --gapplication-service &
 waypaper --restore
