178f6b2969b7f2e869ea4ff98493342c014cd80b
ezntek <eason@ezntek.com>
2024-06-16 15:04:08 +0800

    update

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -5,5 +5,7 @@ hyprpm reload &
 nextcloud &
 #/usr/local/libexec/xdg-desktop-portal -v -r &
 #/usr/local/libexec/xdg-desktop-portal-hyprland -l DEBUG &
-
-/home/ezntek/.local/bin/waypaper --restore
+#hypridle &
+waypaper --restore &
+nm-applet &
+#ibus-daemon -rxRd &
--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -14,7 +14,7 @@ nvim_tree.setup()
 nvim_tree.setup({
   sort_by = "case_sensitive",
   view = {
-    width = 30,
+    width = 24,
   },
   renderer = {
     group_empty = true,
